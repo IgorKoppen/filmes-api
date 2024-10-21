cdc03f97d82b133f1761e7e2249bf71e96a68472
igor <igorpasqualino@gmail.com>
2024-10-21 11:53:08 -0300

    Controller user

---
+++ src/main/java/com/github/igorkoppen/filmes/api/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.github.igorkoppen.filmes.api.controller;
+
+
+import com.github.igorkoppen.filmes.api.dto.UserDTO;
+import com.github.igorkoppen.filmes.api.service.UserService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+
+        List<UserDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+        UserDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody @Valid UserDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable @NotNull Long id,
+                                               @RequestBody @Valid UserDTO dto ){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
